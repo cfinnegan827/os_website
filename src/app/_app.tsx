e53ef2d3ab0c39fafd15c5c25156a12a82efb919
Connor Finnegan <cfinnegan827@gmail.com>
2024-11-02 21:48:46 -0400

    add and setup typescript

---
+++ src/app/_app.tsx
@@ -0,0 +1,6 @@
+import "./globals.scss";
+import type { AppProps /*, AppContext */} from 'next/app';
+
+export default function MyApp({ Component, pageProps}: AppProps) {
+    return <Component {...pageProps}/>
+}
